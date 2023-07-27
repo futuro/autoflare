5c2eb6847350f8587ad9c316d871f853929da717
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-07-27 11:11:41 -0500

    Add missing `extract` flag
    My muscle memory for tar flags apparently only extends to the single-letter versions.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ echo "I am $(whoami)"
 # Grab the java binary and put it in our path
 wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
 # When transform the initial directory name into something static, to make it easier to work with
-tar --gzip --transform 's,^[^/]+,jdk,x' --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+tar --gzip --transform 's,^[^/]+,jdk,x' --extract --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
 export PATH=$PWD/jdk/bin:$PATH
 npx shadow-cljs release :main
