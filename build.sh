31756f8f675c08fb927f5fe128930cd181dee82e
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-07-27 11:09:46 -0500

    Attempt to use the JDK from an archive
    This way we don't need sudo, and should hopefully have a reliable JDK at our disposal.

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "I am $(whoami)"
-# Does buildbot have sudo privileges?
-sudo apt-get install temurin-17-jdk
+
+# Grab the java binary and put it in our path
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+# When transform the initial directory name into something static, to make it easier to work with
+tar --gzip --transform 's,^[^/]+,jdk,x' --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+export PATH=$PWD/jdk/bin:$PATH
 npx shadow-cljs release :main
